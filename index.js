f030bd5b0cd16b9bd262a8eb93a6675f91028bdc
waltergun <walter.gunasekaran@gmail.com>
2019-11-09 20:25:07 +0800

    added tooling 1. eslint 2. nyc 3. winston 4. pre-push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+var morgan = require('morgan');
+var logger = require('./src/config/winston');
 const app = express();
 const port = 8010;
 
@@ -11,11 +13,24 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
+app.use(morgan('combined', { stream: logger.stream }));
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // add this line to include winston logging
+    logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    app.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
